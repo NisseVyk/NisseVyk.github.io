40719fd9cb2f7fb7e3414bb88004fa3d3ec43b9e
NisseVyk <nivy0316@storsjogymnasiet.se>
2023-09-14 14:54:25 +0200

    första menyknapp med underkategorier

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Funktion för att toggla synligheten av dropdownmenyn, och för att transformera meny-knappen
 function show(x){
     x.classList.toggle("toggle");
-    console.log("ö")
     document.getElementById("mydropdown").classList.toggle("show")
+}
+function show_uppgifter(x){
+    x.classList.toggle("toggle");
+    document.getElementById("uppgifter_hidden").classList.toggle("uppgifter_shown")
 }
