c4bd1e6309e621ed4d7ee3f7e9ba8e985590af12
NisseVyk <nivy0316@storsjogymnasiet.se>
2023-10-05 14:37:09 +0200

    Bilar färdig

--- Bilar/bilar.js
+++ Bilar/bilar.js
@@ -9,6 +9,29 @@ class Bil {
 
 let billista = []
 
+//Get data from local storage
+async function getDataFromLocalStorage(){
+    try{
+        billista = await JSON.parse(localStorage.getItem("bilarlistan"))
+
+        //Om billistan är tom, Null från localstorage
+        if (billista == null){
+            billista = []
+        }
+
+        lista_bilar_div.innerHTML=""
+
+        billista.forEach(createHtmlBillista)
+
+    }
+    catch(e){
+        console.log(`Fel ${e}`)
+    }
+}
+
+getDataFromLocalStorage()
+
+
 function addButtonClick(){
     //console.log(`Fabrikat: ${brand.value} Färg: ${color.value}`)
     const now = Date.now()
@@ -43,7 +66,9 @@ const createHtmlBillista = (bil) => {
 
     const bilH2 = document.createElement('H3')
     const bilPcolor = document.createElement('p')
+    bilPcolor.className = "bil_color"
     const bilDelButton = document.createElement('button')
+    bilDelButton.className = "bil_del_button"
 
     bilH2.innerText = `${bil.brand}`
     bilPcolor.innerText = `Färg: ${bil.color}`
@@ -59,3 +84,18 @@ addbutton.addEventListener("click", addButtonClick)
 const brand = document.getElementById("brand")
 const color = document.getElementById("color")
 const lista_bilar_div = document.getElementById("listabilarDiv")
+
+//Deletebil
+let deleteBil = (e) => {
+    console.log("bollar")
+    const ny_billista = billista.filter((o, i) => o.bid != e.target.id)
+
+    billista = ny_billista
+    //skriver till localstorage
+    localStorage.setItem("bilarlistan", JSON.stringify(billista))
+    //läser från LocalStorage och visar sen på websidan den nya listan
+    getDataFromLocalStorage()
+
+}
+
+window.addEventListener("click", deleteBil)
